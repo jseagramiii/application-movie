e36d456389563dd00d7341be635ff7f13a8f5bc4
James Seagram <jseagramiii@gmail.com>
2021-01-22 15:13:38 -0700

    getting ready for deploy

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React, {useState,} from 'react'
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 import Particles from 'react-particles-js'
 import axios from 'axios'
 import SearchBox from './SearchBox'
 import Details from './Details'
 import MovieCard from './MovieCard'
-
 
 function App() {
   const apiKey = process.env.REACT_APP_API_KEY
@@ -13,21 +12,23 @@ function App() {
   const [loading, setLoading] = useState(false)
   const [details, setDetails] = useState(false)
   const [show, setShow] = useState(false)
-  
-// axios request movies in OMDB database
+
+  // axios request movies in OMDB database
   const searchMovies = async (text) => {
     setLoading(true)
 
-    let res = await axios.get(`http://www.omdbapi.com/?apikey=${apiKey}&s=${text}`)
+    let res = await axios.get(
+      `http://www.omdbapi.com/?apikey=${apiKey}&s=${text}`
+    )
 
     setMovies(res.data.Search)
     setLoading(false)
   }
-  
+
   const particlesOptions = {
-    'particles': {'number': {'value': 250}, 'size': {'value': 0.3}}
-   }
-  
+    particles: { number: { value: 200 }, size: { value: 0.3 } },
+  }
+
   return (
     <div className='app'>
       <Particles params={particlesOptions} className='particles' />
@@ -35,20 +36,22 @@ function App() {
       <SearchBox searchMovies={searchMovies} loading={loading} />
       <div className='grid'>
         {movies.map((movie, index) => (
-          <MovieCard movie={movie} key={index} show={show} setShow={setShow} setDetails={setDetails} loading={loading} setLoading={setLoading} />
-          ))} 
+          <MovieCard
+            movie={movie}
+            key={index}
+            show={show}
+            setShow={setShow}
+            setDetails={setDetails}
+            loading={loading}
+            setLoading={setLoading}
+          />
+        ))}
       </div>
-      {show ?
-      <Details  
-      details={details}
-      setShow={setShow}
-      show={show}
-      /> 
-      : null 
-      }
+      {show ? (
+        <Details details={details} setShow={setShow} show={show} />
+      ) : null}
     </div>
   )
 }
 
-
-export default App;
+export default App
